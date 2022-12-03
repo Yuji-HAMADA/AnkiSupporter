f181c6da477e863236a5b4a0e3e23bbbec011dfa
Yuji Hamada <yuji.tky.ykhm@gmail.com>
2022-12-04 08:44:30 +0900

    Initial commit

---
+++ app/src/main/java/com/yuji/ankisupporter/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.yuji.ankisupporter
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.yuji.ankisupporter.ui.theme.AnkiSupporterTheme
+
+class MainActivity : AppCompatActivity() {
+    private val _app_TAG = "Main Activity"
+
+    companion object {
+        const val REQUEST_RECORD_AUDIO_PERMISSION = 1001
+    }
+
+    // Requesting permission to RECORD_AUDIO
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+        if (!permissionToRecordAccepted) finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.v(_app_TAG, "AnkiSupporter Start")
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+
+        setContent {
+            AnkiSupporterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    UserInterface(this)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UserInterface(context: Context) {
+    Column {
+        Button(onClick = { WordManager.onRecord(context) }) {
+            Text(stringResource(R.string.record))
+        }
+        Button(onClick = { /*wordPlayer.onStop()*/ }) {
+            Text(stringResource(R.string.stop))
+        }
+        Button(onClick = { WordManager.onPlay() }) {
+            Text(stringResource(R.string.playback))
+        }
+    }
+}
+
+@Preview(name = "My Preview", showSystemUi = true)
+@Composable
+fun CardPreview() {
+    AnkiSupporterTheme {
+//        Greeting()
+    }
+}
---
+++ app/src/main/java/com/yuji/ankisupporter/WordManager.kt
@@ -0,0 +1,93 @@
+package com.yuji.ankisupporter
+
+import android.content.Context
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.util.Log
+import java.io.IOException
+
+object WordManager {
+
+    private var fileName: String = ""
+    private var player: MediaPlayer? = null
+    private var recorder: MediaRecorder? = null
+
+    private var mStartRecording : Boolean = true
+    private var mStartPlaying : Boolean = true
+
+    private const val LOG_TAG = "Word Manager"
+
+    private fun startRecording(context: Context) {
+        // Record to the external cache directory for visibility
+        if (fileName == "") {
+            fileName = "${context.externalCacheDir?.absolutePath}/audiorecordtest.3gp"
+            Log.v(LOG_TAG, "filePath is set")
+        }
+
+        recorder = MediaRecorder(context).apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+
+            start()
+        }
+    }
+
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
+    }
+
+    fun init(context: Context) {
+
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+        }
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+    }
+
+    fun onRecord(context: Context) {
+        if (mStartRecording) {
+            Log.v(LOG_TAG, "start recording")
+            startRecording(context)
+        } else {
+            Log.v(LOG_TAG, "stop recording")
+            stopRecording()
+        }
+        mStartRecording = !mStartRecording
+    }
+
+    fun onPlay() {
+        if (mStartPlaying) {
+            Log.v(LOG_TAG, "start playing")
+            startPlaying()
+        } else {
+            Log.v(LOG_TAG, "stop playing")
+            stopPlaying()
+        }
+        mStartPlaying = !mStartPlaying
+    }
+}
