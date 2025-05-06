dd45ea51b0ec7414b8b48e575a8312513dd424d0
Yuji HAMADA <yuji.tky.ykhm@gmail.com>
2025-05-06 13:47:02 +0900

    Upgraded to the latest libraries. Used libs.versions.toml.

--- app/src/main/java/com/yuji/ankisupporter/AnkiSupporterApp.kt
+++ app/src/main/java/com/yuji/ankisupporter/AnkiSupporterApp.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
